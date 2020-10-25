e8e5880408c9b0b82b79a23299253d8e5d1d617f
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-10-25 20:11:10 +0100

    fire to firestore

---
+++ fire/fire.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	stan "github.com/nats-io/stan.go"
+)
+
+const subject = "room"
+
+var scon stan.Conn
+var client *firestore.Client
+var ctx context.Context
+
+func STANConnect(_ stan.Conn, _ error) {
+	time.Sleep(3 * time.Second)
+	sc, err := stan.Connect("measures", "gun", stan.NatsURL("nats://rpi3:4222"), stan.SetConnectionLostHandler(STANConnect))
+	if err != nil {
+		log.Panicln(err)
+	}
+	scon = sc
+}
+
+func handleMsg(msg *stan.Msg) {
+	var payload map[string]interface{}
+	payload = make(map[string]interface{})
+	ref := client.Collection("room-measures")
+	json.Unmarshal(msg.Data, &payload)
+	_, _, err := ref.Add(ctx, payload)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+	forever := make(chan bool)
+	STANConnect(nil, nil)
+	defer scon.Close()
+	log.Println("Connected")
+	// firebase
+	ctx = context.Background()
+	app, err := firebase.NewApp(ctx, nil)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+	client, err = app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+	scon.Subscribe(subject, handleMsg, stan.DurableName("1"), stan.DeliverAllAvailable())
+	<-forever
+}
---
+++ fire/go.mod
@@ -0,0 +1,12 @@
+module fire
+
+go 1.15
+
+require (
+	cloud.google.com/go/firestore v1.3.0
+	cloud.google.com/go/storage v1.12.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/nats-io/stan.go v0.7.0
+	google.golang.org/api v0.33.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
